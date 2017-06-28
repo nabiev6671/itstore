39fc9aefc15b8cd277d7154c00aee05b1f3360a0
nabiev6671 <nabiev6671@ya.ru>
2017-06-28 17:59:05 +0500

    Доделал функционал слайдера

--- app/js/main_slider.js
+++ app/js/main_slider.js
@@ -1,29 +1,29 @@
-////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//
-//
-//
-////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Данный файл  содержит в себе модуль mainSlider. Функция запускается из файла main.js, перед тем как
+// зпустить этот фрагмент кода main.js проверяет DOM дерево на наличие необходимость элементов
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 var mainSlider = (function () {
 	// Инициализация
 	var init = function () {
 			_setUpListners();
+			_controller.sliderStart('start');  // раскоментрий для автоматического старта слайдера
 		};
 
 	// _setUpListners - прослушка
-	var _setUpListners = function (){
-		doc.find("#buttonHeader").click( function () {
-			_controller.newPosIn(calcObj);
-		});
-		doc.find("#mainSlider").click( function () {
-			_controller.disNewIn();
-		});
+	var _setUpListners = function (){																							// Прослушка событий _setUpListners. Данная функция содержит в себе проуслушку
+			doc.find('#sliderControllLeft').click(function () {												// кнопок для переключения слайдера влево или вправо. Попутно
+				if (calcObj.newPos > -lastSliderPos ) {																	// в функции проверяется возможность передвижения слайдера в
+					_controller.step('left');																							// в нужную сторону в зависимости от моложения контейнера для слайдов
+				}
+			});
+			doc.find('#sliderControllRight').click(function () {
+				console.log(calcObj.newPos);
+				if (calcObj.newPos < firstSliderPos) {
+					_controller.step('right');
+				}
+			});
 		};
 
 	var
@@ -41,10 +41,14 @@ var mainSlider = (function () {
 			imgW = img.width(), // ширина картинки
 
 			imgWSum = slidW * slid.length, // сумма всех ширин слайдов
-			imgBoxPos = imgBox.position().left; //позиция контейнера со слайдами относительно левого края
+			imgBoxPos = imgBox.position().left, //позиция контейнера со слайдами относительно левого края
+			sliderTimer, //переменная для сохранения Setinterval для слайдера
+			firstSliderPos = slid.first().position().left, // сохраняем положение первого слайда относительно левого края
+			lastSliderPos = slid.last().position().left; // сохраняем положение последнего слайда относительно левого края
+
 
 		var
-			// calcObj - для передачи данных в функцию calcFunc
+			// calcObj - хранит в себе настоящее состояние слайдера
 			calcObj = {
 				name 	 : 'calcObj', // имя объекта
 				dis    : 'left', // направление движения
@@ -52,37 +56,77 @@ var mainSlider = (function () {
 				step   : slidW // шаг на который будет произведен сдвиг слайдера
 			};
 
-		var	_controller = (function () {
+		// Функция _view отвечает за внешиний вид слайдера и за выполнение анимации
+		var _view = function (pos) {
+			imgBox.animate({
+				left : pos
+			})
+		};
+
+
+		var	_controller = (function () {																						// Функция _controller является основной функцией для слайдера
+
+			var step = function (dir) {																								// функция step срабатывает когда пользователь нажимает
+				sliderStart('stop'); 																										// на кнопки влево или вправо в слайдере. Функция останавливает
+				if (dir === 'left') {																										// фвтоматическую прокурутку слайдера, меняет направление прокрутки слайдера
+					calcObj.dis = 'left';																									// в зависисмости от нажатой кнопки.
+				} else if (dir === 'right') {
+					calcObj.dis = 'right';
+				}
+				newPosFunc(calcObj);
+				_view(calcObj.newPos);
+			};
+
+			var sliderStart = function (action) {																			// sliderStart - функция для автоматического старта прокурутки слайдера
+				if (action === 'start') {																								// в зависисмости от значений 'action' функция запускает таймер для прокурутки
+					 sliderTimer = setInterval (function () {															// или останавливает
+						newPosFunc(calcObj);
+						_view(calcObj.newPos);
+						checkFollowingSlide();
+					}, 5000);
+				} else if (action === 'stop') {
+					clearInterval(sliderTimer);
+				}
+			}
+
+			var checkFollowingSlide = function (requesterName) {											// checkFollowingSlide - функция проверки. Она проверет есть ли еще слайды
+					if (calcObj.newPos <= -lastSliderPos ) {															// для того чтобы перелистныть слайдер вправо или влево
+						disNewFunc();
+					} else if (calcObj.newPos >= firstSliderPos) {
+						disNewFunc();
+					}
+			}
+
+
+			// функция newPosFunc рассчитывает новое расположение контейнера со слайдами
+			// относительно левого края.
+			var newPosFunc = function (obj) {
+				if (obj.dis === 'left') {
+					obj.newPos = obj.newPos - obj.step;
+				} else if (obj.dis === 'right') {
+					obj.newPos = obj.newPos + obj.step;
+				}
+			};
 
 			// функция disNewFunc переключает направление движения слайдера
 			var disNewFunc = function () {
 				if (calcObj.dis === 'left') {
 					calcObj.dis = 'right'
-					console.log(calcObj);
 				} else {
 					calcObj.dis = 'left'
-					console.log(calcObj);
-				}
-			};
-
-			// функция newPosFunc расчитывает новое расположение контейнера со слайдами
-			// относительно левого края.
-			var newPosFunc = function (obj) {
-				if (obj.dis === 'left') {
-					obj.newPos = obj.newPos - obj.step;
-					console.log('step Left');
-				} else if (obj.dis === 'right') {
-					obj.newPos = obj.newPos + obj.step;
-					console.log('step right');
 				}
 			};
 
 			return {
 				disNewIn : disNewFunc,
-				newPosIn : newPosFunc
+				newPosIn : newPosFunc,
+				step : step,
+				sliderStart : sliderStart,
+				checkFollowingSlide : checkFollowingSlide
 			}
 
 		})();
+
 
 
 return {
